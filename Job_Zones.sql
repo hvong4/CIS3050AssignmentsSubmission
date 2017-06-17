efab611fab853b05d34058a1b12f3242d25ce17b
hvong4 <huyyivong1528@gmail.com>
2017-06-17 13:52:21 -0700

    Update Job_Zones.sql
    I think this table is also in the third normal form because even though there it has no primary key mentions (other than foreign keys).  There is no duplication in the table and no multiple values for each column.

--- Job_Zones.sql
+++ Job_Zones.sql
@@ -5,6 +5,8 @@ CREATE TABLE job_zone_reference (
   education CHARACTER VARYING(500) NOT NULL,
   experience CHARACTER VARYING(300) NOT NULL,
   level_of_job_training CHARACTER VARYING(300) NOT NULL);
+  
+  /* FOREIGN KEY (onetsoc_code) REFERENCES occupation_data(onetsoc_code), FOREIGN KEY (job_zone) REFERENCES job_zone_reference(job_zone));*/
 /*! COMMIT */;
 /*! START TRANSACTION */;
 
